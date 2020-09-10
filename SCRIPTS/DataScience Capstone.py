e85025a0976553c45fa506ab3d93acc44ea732e6
darthur2019 <david.kofi.arthur@gmail.com>
2020-09-09 20:07:11 -0400

    added new files and using VSCode

---
+++ SCRIPTS/DataScience Capstone.py
@@ -0,0 +1,42 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+import pandas as pd
+
+
+# %%
+df = pd.read_csv('Data-Collisions.csv')
+
+
+# %%
+df.describe()
+
+
+# %%
+df.info()
+
+
+# %%
+df[['ROADCOND']].count()
+
+
+# %%
+194673-189661
+
+
+# %%
+df['SEVERITYCODE'].value_counts()
+
+
+# %%
+df.describe(include='all')
+
+
+# %%
+print("I am David")
+
+
+# %%
+
+
+
