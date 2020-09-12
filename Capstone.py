15679b4bc62e416008d8a6ae2cfec48d5b8bb859
darthur2019 <david.kofi.arthur@gmail.com>
2020-09-12 13:08:41 -0400

    Updated Name of Notebook

---
+++ Capstone.py
@@ -0,0 +1,28 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.6.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Data Science Capstone
+#
+# This notebook will be used for the IBM Data Science Certification Capstone
+
+# %%
+import pandas as pd
+import numpy as np
+
+# %%
+print("Hello Capstone Project Course")
+
+# %%
